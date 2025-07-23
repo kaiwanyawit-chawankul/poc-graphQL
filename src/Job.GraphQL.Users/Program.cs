87c33529ba86633ad19ec279b6b532c567e74f4b
Kaiwanyawit Chawankul <kaiwanyawit.c@thoughtworks.com>
2025-07-23 21:45:06 +0700

    add mock user with swagger

--- src/Job.GraphQL.Users/Program.cs
+++ src/Job.GraphQL.Users/Program.cs
@@ -2,6 +2,8 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 builder.Services.AddOpenApi();
 
 var app = builder.Build();
@@ -9,6 +11,8 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
+    app.UseSwagger();
+    app.UseSwaggerUI();
     app.MapOpenApi();
 }
 
